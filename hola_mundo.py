dbbaa904c1cdf0bac015be1ff3767b2872327cb0
Ledezmahub <ledezmajeremias222@outlook.com>
2026-05-22 18:38:24 -0400

    feat: agregar primer programa hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,7 @@
+# Mi primer programa en Python
+# Este es un comentario - Python lo ignora
+
+print("¡Hola, Mundo!")
+print("Bienvenido a Python")
+
+# print() es una función que muestra texto en la pantalla
